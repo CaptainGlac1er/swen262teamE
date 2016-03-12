9413d6b2dde6e21cba37ce61b35fe8eecdf962ee
Wizeman02 <jxj8200@rit.edu>
2016-03-12 17:41:59 -0500

    Added setWorth parameter to StockParent for bull/bear market

--- src/FTPS/StockParent.java
+++ src/FTPS/StockParent.java
@@ -12,14 +12,13 @@ public class StockParent {
 
     public StockParent (String inName, String inAbbr, String inIndex, double inWorth)
     {
-
         stockName = inName;
         stockAbbr = inAbbr;
         stockIndex = inIndex;
         stockWorth = inWorth;
     }
 
-    public void setWorth
+    public void setWorth(double inWorth ){ stockWorth = inWorth; }
     public double getWorth(){
         return stockWorth;
     }
