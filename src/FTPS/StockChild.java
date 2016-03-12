c7f45d1147fbefdb7377f56066a3ad60246f6db2
Wizeman02 <jxj8200@rit.edu>
2016-03-12 17:35:44 -0500

    Changing parent/child stock classes to be inheritance correct.

--- src/FTPS/StockChild.java
+++ src/FTPS/StockChild.java
@@ -6,64 +6,71 @@ import java.util.List;
 /**
  * Created by Josh on 3/3/2016.
  */
-public class StockChild extends StockParent implements  Stock{
+public class StockChild extends StockParent implements Stock {
 
     private int stockCount;
-    private String stockName;
-    private String stockAbbr;
-    private String stockIndex;
-    private double stockWorth;
     private List<StockChild> indexStocks;
 
-    public StockChild (String inName, String inAbbr, String inIndex, int inCount, double inWorth){
-        super (inName, inAbbr, inIndex, inWorth);
+    public StockChild(String inName, String inAbbr, String inIndex, int inCount, double inWorth) {
+        super(inName, inAbbr, inIndex, inWorth);
         stockCount = inCount;
-        stockName =  inName;
+        stockName = inName;
         stockAbbr = inAbbr;
         stockWorth = inWorth;
         stockIndex = inIndex;
         indexStocks = new ArrayList<StockChild>();
     }
-    public List<StockChild> getIndexStocks(){
+
+    public List<StockChild> getIndexStocks() {
         return indexStocks;
     }
-    public void addIndexStock(StockChild s){
+
+    public void addIndexStock(StockChild s) {
         indexStocks.add(s);
     }
-    public void removeIndexStock(StockChild s){
+
+    public void removeIndexStock(StockChild s) {
         indexStocks.remove(s);
     }
+
     //Return total worth of owned stock
     public double GetTotWorth() {
         return (super.getWorth() * stockCount);
     }
 
     //Return the count of stocks owned
-    public int GetCount(){
+    public int GetCount() {
         return stockCount;
     }
 
     //Increment the count of stocks owned
-    public void IncCount(){
+    public void IncCount() {
         stockCount++;
     }
+
     //override for an amt
-    public void IncCount(int quantity){
-        stockCount+=quantity;
+    public void IncCount(int quantity) {
+        stockCount += quantity;
     }
+
     //Decrement the count of stocks owned
     public void DecCount() {
         stockCount--;
     }
+
     //override for an amt
     public void DecCount(int quantity) {
         stockCount -= quantity;
     }
-    public String getStockAbbr(){
+
+    public String getStockAbbr() {
         return ("Abbr :" + stockAbbr);
     }
-    public String getStockIndex(){
+
+    public String getStockIndex() {
         return ("Index :" + stockIndex);
     }
+
+    public
 
 }
--- src/FTPS/StockParent.java
+++ src/FTPS/StockParent.java
@@ -4,9 +4,23 @@ package FTPS;
  * Created by CaptainGlac1er on 3/4/2016.
  */
 public class StockParent {
-    public StockParent (String inName, String inAbbr, String inIndex, double inWorth){
+
+    private String stockName;
+    private String stockAbbr;
+    private String stockIndex;
+    private double stockWorth;
+
+    public StockParent (String inName, String inAbbr, String inIndex, double inWorth)
+    {
+
+        stockName = inName;
+        stockAbbr = inAbbr;
+        stockIndex = inIndex;
+        stockWorth = inWorth;
     }
+
+    public void setWorth
     public double getWorth(){
-        return 0;
+        return stockWorth;
     }
 }
